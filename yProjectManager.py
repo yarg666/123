87b0907750a47959f08a9b0a84c1725cf2ecc71e
yarg666 <julienmargelin@gmail.com>
2018-03-04 19:21:57 +0100

    okkkk

--- yProjectManager.py
+++ yProjectManager.py
@@ -4,39 +4,64 @@
 
 #def createInterface():
 #    return yProjectManager.createInterface()
-
-
-
-
-from hutil.Qt import QtWidgets
+from PySide2 import QtWidgets
 import os
 import hou
+print "Hello from yProjectManager"
 
-print "Hello yProjectManager"
+class yProjectManagerClass(QtWidgets.QWidget): 
 
-def openScene(hipName):
+	def __init__(self):
+		super (yProjectManagerClass,self).__init__()
+		#variables de projet
+		self.proj="/media/vfx/"
+		#les widgets
+		self.roots=QtWidgets.QLineEdit(self.proj)
+		self.filtrer=QtWidgets.QLineEdit()
+		self.filtrer.setPlaceholderText('project filter')
+		self.refreshBouton=QtWidgets.QPushButton("refresh")
+		self.listWidget = QtWidgets.QListWidget()
+		self.createInterface()
+	    #le layout
+		mainLayout=QtWidgets.QVBoxLayout()
+		mainLayout.addWidget(self.roots)
+		mainLayout.addWidget(self.filtrer)
+		mainLayout.addWidget(self.refreshBouton)
+		mainLayout.addWidget(self.listWidget)
+		self.setLayout(mainLayout)
 
-    hipFile=hipName.data()
-     
-    #open hipFile
-    hou.hipFile.load(hipFile)
-    
-def createInterface():
+	def openScene(self,hipName):
+	    hipFile=hipName.data()
+	    #open hipFile
+	    hou.hipFile.load(hipFile)
 
-    proj = "/media/vfx/"
-    widget = QtWidgets.QLabel(proj)
-    listWidget = QtWidgets.QListWidget()
-    
-    
-    for dir in os.walk(proj).next()[1]:
-        projLevelOne = proj+dir
-        for file in os.listdir(projLevelOne):
-            if file.endswith('.hip'):
-                hipPath = projLevelOne+"/"+file 
-                #print hipPath
-                #print file
-                listWidget.addItem(hipPath)
-                
-    listWidget.doubleClicked.connect(openScene)
-    
-    return listWidget
+	def createInterface(self):
+
+		self.proj=self.roots.text()
+		self.listWidget.clear()
+		#boucle qui list les hip dans les dossiers
+		tempList=[]
+		for dir in os.walk(self.proj).next()[1]:
+			projLevelOne = self.proj+dir
+			for file in os.listdir(projLevelOne):
+				if file.endswith('.hip'):
+					hipPath = projLevelOne+"/"+file
+					tempList.append(hipPath)
+		#triage de la liste
+		sortList=sorted(tempList)
+		# systeme de filtres par mot
+		selectionFilter=self.filtrer.text()
+		listeFlitre=[]
+		# test if there is a key word to filter the list with
+		if len(selectionFilter) == 0:
+			listeFlitre=sortList
+		else :
+			for listElement in sortList:
+				if selectionFilter in listElement:
+					listeFlitre.append(listElement)
+		# assigne la liste filtrer au widget liste
+		for hipPathSorted in listeFlitre:
+			self.listWidget.addItem(hipPathSorted)
+		# set widget connection
+		self.listWidget.doubleClicked.connect(self.openScene)
+		self.refreshBouton.released.connect(self.createInterface)
